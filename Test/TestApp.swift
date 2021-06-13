472033fd84d9709536e83c792083248d27dc2760
Angry Coding <mailidanielifechukwu@gmail.com>
2021-06-13 16:34:40 +0100

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by ifechukwu daniel; on 13/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
